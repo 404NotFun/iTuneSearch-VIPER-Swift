d5c0e9b745e19a0193b4c4071fac2f81b6c8e115
TMAker <funnot404@gmail.com>
2018-08-08 12:36:17 +0800

    Initial Commit

---
+++ M800_assignment/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  M800_assignment
+//
+//  Created by Jason Tsai on 2018/8/8.
+//  Copyright © 2018年 TMAker. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
